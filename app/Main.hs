724d035b612fed4e07e9767f43e4dc5d273810bc
Ori Levenglick <61731312+o11k@users.noreply.github.com>
2025-07-20 06:57:13 +0300

    Read argv

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import System.Environment (getProgName, getArgs)
+import System.Exit (die)
+
+defaultProgName :: String
+defaultProgName = "dump-hie"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- getArgs
+    prog <- getProgName
+    let progName = if null prog then defaultProgName else prog
+    case args of
+        [filename]  -> putStrLn filename
+        _           -> die $ "Usage: " ++ progName ++ " <hie-path>"
