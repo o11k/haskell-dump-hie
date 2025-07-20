9c6f550895b63a2e0bb86be5bcf16a87ef42972b
Ori Levenglick <61731312+o11k@users.noreply.github.com>
2025-07-20 07:11:13 +0300

    Read .hie file

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,9 @@ module Main where
 
 import System.Environment (getProgName, getArgs)
 import System.Exit (die)
+import GHC.Iface.Ext.Binary (readHieFile, HieFileResult (..))
+import GHC.Types.Name.Cache (initNameCache)
+import GHC.Iface.Ext.Types (HieFile(..))
 
 defaultProgName :: String
 defaultProgName = "dump-hie"
@@ -11,6 +14,10 @@ main = do
     args <- getArgs
     prog <- getProgName
     let progName = if null prog then defaultProgName else prog
-    case args of
-        [filename]  -> putStrLn filename
-        _           -> die $ "Usage: " ++ progName ++ " <hie-path>"
+    if length args /= 1 then die $ "Usage: " ++ progName ++ " <hie-path>"
+    else do
+        let filename = head args
+        nameCache <- initNameCache 'A' []
+        hieFileResult <- readHieFile nameCache filename
+        let hieFile = hie_file_result hieFileResult
+        putStrLn $ hie_hs_file hieFile
